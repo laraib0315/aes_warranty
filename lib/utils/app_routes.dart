a2bebf1100031a5d0c3d5a52395810e001633c80
laraib0315 <laraibhussain131@gmail.com>
2026-06-13 03:04:00 +0500

    fix gradle, remove heavy packages, regenerate adapters

--- lib/utils/app_routes.dart
+++ lib/utils/app_routes.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import '../pages/login_page.dart';
+import '../pages/main_scaffold.dart';
 import '../pages/web_login_page.dart';
 import '../pages/web_main_scaffold.dart';
 
 class AppRoutes {
+  // Route ke names (String constants)
   static const String login = '/';
   static const String home = '/home';
   static const String webLogin = '/web_login';
   static const String webHome = '/web_home';
 
+  // Mobile aur Web dono ke routes ka map
   static Map<String, WidgetBuilder> routes = {
-    login: (context) => const WebLoginPage(), // Use web version by default
-    home: (context) => const WebMainScaffold(),
-    webLogin: (context) => const WebLoginPage(),
-    webHome: (context) => const WebMainScaffold(),
+    // Mobile Version Routes
+    login: (context) => const LoginPage(), // mobile login
+    home: (context) => const MainScaffold(), // mobile home
+
+    // Web Version Routes
+    webLogin: (context) => const WebLoginPage(), // web login
+    webHome: (context) => const WebMainScaffold(), // web home
   };
 }
