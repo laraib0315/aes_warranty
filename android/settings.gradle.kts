bb991740bdfd5c35309dd4b239c0272b8d554cff
laraib0315 <laraibhussain131@gmail.com>
2026-06-12 06:14:02 +0500

    fix gradle, remove heavy packages, regenerate adapters

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,12 +1,16 @@
 pluginManagement {
-    val flutterSdkPath =
-        run {
-            val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+    def flutterSdkPath = {
+        def properties = new Properties()
+        def propertiesFile = settingsDir.parentFile.toPath().resolve("local.properties").toFile()
+        if (propertiesFile.exists()) {
+            propertiesFile.withReader("UTF-8") { reader -> properties.load(reader) }
         }
+        def sdkPath = properties.getProperty("flutter.sdk")
+        if (sdkPath == null) {
+            throw new GradleException("Flutter SDK not found in local.properties")
+        }
+        return sdkPath
+    }()
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
 
@@ -18,9 +22,10 @@ pluginManagement {
 }
 
 plugins {
-    id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "9.0.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.3.20" apply false
+    id "dev.flutter.flutter-plugin-loader" version "1.0.0"
+    id "com.android.application" version "8.1.0" apply false
+    // Yeh line Built-in Kotlin support enable karegi:
+    id "org.jetbrains.kotlin.android" version "1.8.22" apply false
 }
 
-include(":app")
+include ":app"
